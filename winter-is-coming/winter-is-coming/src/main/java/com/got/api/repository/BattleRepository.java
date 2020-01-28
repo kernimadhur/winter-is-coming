34c195fe10b68860a708ead3355ccdd890baac9e
kernimadhur <60298266+kernimadhur@users.noreply.github.com>
2020-01-29 00:48:15 +0530

    used CrudRepository
    Removed the repo impl class

--- winter-is-coming/winter-is-coming/src/main/java/com/got/api/repository/BattleRepository.java
+++ winter-is-coming/winter-is-coming/src/main/java/com/got/api/repository/BattleRepository.java
@@ -3,12 +3,20 @@ package com.got.api.repository;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
 import com.got.api.model.ListModel;
 
-public interface BattleRepository {
+
+public interface BattleRepository extends CrudRepository<ListModel, String>  {
 	
-	 List<ListModel> findList();
-	 int count();
-	 Optional<ListModel> getBattleById(String id);
-	  Optional<ListModel> checkBattle(String id);
+	public static final String FIND_PROJECTS ="SELECT location,region FROM battle_info";
+			
+	@Query(value = FIND_PROJECTS, nativeQuery = true)
+	public List<Object[]> findEmp_name();
+	
+	 int counts();
+	 ListModel findByBattle(String id);
+	 Optional<ListModel> checkBattle(String id);
 }
